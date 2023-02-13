2128fd10ded6d4ea58832f0612c17e0f806c9a31
sihar <luq5nvd@gmail.com>
2023-02-13 20:53:45 +0700

    create new activity, item_movie_list

---
+++ app/src/main/java/com/example/moviefinder/ItemMovieListActivity.java
@@ -0,0 +1,14 @@
+package com.example.moviefinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ItemMovieListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.item_movie_list);
+    }
+}
